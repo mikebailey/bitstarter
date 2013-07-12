a46609525a6a2278fde378b028d49c9a6567e427
mikebailey <mikebailey@alumni.stanford.edu>
2013-07-12 06:03:08 +0000

    fixed web.js

--- web.js
+++ web.js
@@ -2,7 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var text = fs.readFileSync('index.html');
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
 
 app.get('/', function(request, response) {
   response.send('abc');
